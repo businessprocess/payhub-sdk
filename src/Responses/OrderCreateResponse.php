d6cc7061fcd8499350de9719aa78ba5baf01dd5b
Oleksandr Hurskyi <maveriks.echo@gmail.com>
2023-11-22 20:27:02 +0200

    add params to create response

--- src/Responses/OrderCreateResponse.php
+++ src/Responses/OrderCreateResponse.php
@@ -10,6 +10,12 @@ class OrderCreateResponse
 
     protected string $checkoutId;
 
+    protected array $checkoutData = [];
+
+    protected array $paymentData = [];
+
+    protected int $status;
+
     protected ?int $orderId;
 
     protected DateTime $dateExpired;
@@ -23,6 +29,9 @@ class OrderCreateResponse
         $this->link = $params['link'];
         $this->dateExpired = date_create_from_format('Y-m-d H:i:s', $params['date_expired']);
         $this->message = $params['message'] ?? null;
+        $this->checkoutData = $params['checkout_data'] ?? [];
+        $this->paymentData = $params['payment_data'] ?? [];
+        $this->status = $params['status'] ?? 0;
     }
 
     public function toArray(): array
@@ -30,6 +39,9 @@ class OrderCreateResponse
         return [
             'checkoutId' => $this->getCheckoutId(),
             'orderId' => $this->getOrderId(),
+            'checkout_data' => $this->getCheckoutData(),
+            'payment_data' => $this->getPaymentData(),
+            'status' => $this->getStatus(),
             'link' => $this->getLink(),
             'dateExpired' => $this->getDateExpired(),
             'message' => $this->getMessage(),
@@ -59,5 +71,25 @@ class OrderCreateResponse
     public function getOrderId(): ?int
     {
         return $this->orderId;
+    }
+
+    public function getCheckoutData(): array
+    {
+        return $this->checkoutData;
+    }
+
+    public function getStatus(): int
+    {
+        return $this->status;
+    }
+
+    public function isPaid(): bool
+    {
+        return (int)$this->getStatus() === 1;
+    }
+
+    public function getPaymentData(): array
+    {
+        return $this->paymentData;
     }
 }
