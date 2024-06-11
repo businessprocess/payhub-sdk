545c4997d9a5f348f53e56179e081a8a20a9d652
Hurskyi <maveriks.echo@gmail.com>
2024-06-11 13:10:20 +0300

    update payment method model

--- src/Models/PaymentMethod.php
+++ src/Models/PaymentMethod.php
@@ -10,6 +10,18 @@ class PaymentMethod extends Base
 
     protected string $key;
 
+    protected bool $active;
+
+    protected array $countries;
+
+    protected bool $confirmation;
+
+    protected bool $real_money;
+
+    protected bool $credit;
+
+    protected string $category;
+
     public function getId(): mixed
     {
         return $this->id;
@@ -38,5 +50,55 @@ class PaymentMethod extends Base
     public function setKey(string $key): void
     {
         $this->key = $key;
+    }
+
+    public function getCountries(): array
+    {
+        return $this->countries;
+    }
+
+    public function setCountries(array $countries): void
+    {
+        $this->countries = $countries;
+    }
+
+    public function isConfirmation(): bool
+    {
+        return $this->confirmation;
+    }
+
+    public function setConfirmation(bool $confirmation): void
+    {
+        $this->confirmation = $confirmation;
+    }
+
+    public function isRealMoney(): bool
+    {
+        return $this->real_money;
+    }
+
+    public function setRealMoney(bool $real_money): void
+    {
+        $this->real_money = $real_money;
+    }
+
+    public function isCredit(): bool
+    {
+        return $this->credit;
+    }
+
+    public function setCredit(bool $credit): void
+    {
+        $this->credit = $credit;
+    }
+
+    public function getCategory(): string
+    {
+        return $this->category;
+    }
+
+    public function setCategory(string $category): void
+    {
+        $this->category = $category;
     }
 }
